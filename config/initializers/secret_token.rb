68aab1718169b674e20ad24ad7303500436c5f22
Avenel <rambo256@arcor.de>
2011-11-26 00:19:28 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyChart::Application.config.secret_token = '391fd95aaabc27b57c5247a4c8da963b282099428cc650d02c9158366f51fca79c5a5c1cd01a94c795e59d5b0214c9298809e3656e466fc24a6cb4e3b62ee176'
